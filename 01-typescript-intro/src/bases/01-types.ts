e66f97c304cd609b95cd6f69f27e3ffe812205b4
Javier Flores <jffc.dev@gmail.com>
2024-12-16 01:40:11 -0500

    feat: types in arrays

---
+++ 01-typescript-intro/src/bases/01-types.ts
@@ -0,0 +1,33 @@
+export let name = 'Javier FLores'
+export const age: number = 22
+export const isValid: boolean = true
+
+name = 'Fernando'
+
+export const pokemonIds: number[] = [1,20,12,33]
+pokemonIds.push('data')
+
+export interface Pokemon {
+    id: number
+    name: string
+    age?: number
+}
+
+export const pikachu: Pokemon = {
+    id: 1,
+    name: 'pikachu'
+}
+
+export const charmander: Pokemon = {
+    id: 2,
+    name: "charmander",
+    age: 23
+}
+
+// console.log(pikachu)
+// console.log(charmander)
+
+export const pokemons: Pokemon[] = []
+pokemons.push(pikachu, charmander, 1)
+
+console.log(pokemons)
