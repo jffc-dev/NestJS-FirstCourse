cb97a0d4268e6e176dc691575aa0b68f86f1464d
Javier Flores <jffc.dev@gmail.com>
2024-12-16 12:07:56 -0500

    feat: liskov substitution added

--- 01-typescript-intro/src/api/pokeApi.adapter.ts
+++ 01-typescript-intro/src/api/pokeApi.adapter.ts
@@ -1,12 +1,28 @@
 import axios from 'axios'
 
-export class PokeApiAdapter {
+export interface HttpAdapter {
+    get<T>(url: string): Promise<T>
+    // post<T>(url: string, data: any): Promise<T>
+    // put<T>(url: string, data: any): Promise<T>
+    // patch<T>(url: string, data: any): Promise<T>
+    // delete<T>(url: string): Promise<T>
+}
+
+export class PokeApiFectchAdapter implements HttpAdapter {
+    async get<T>(url: string): Promise<T>{
+        const resp = await fetch(url)
+        const data: T = await resp.json()
+        return data
+    }
+}
+
+export class PokeApiAdapter implements HttpAdapter {
 
     private readonly axios = axios;
 
-    async get(url: string){
-        const rsp = await this.axios.get(url)
-        return rsp
+    async get<T>(url: string): Promise<T>{
+        const {data} = await this.axios.get<T>(url)
+        return data
     }
 
     async post(url: string, data: any){
--- 01-typescript-intro/src/bases/04-injection.ts
+++ 01-typescript-intro/src/bases/04-injection.ts
@@ -1,5 +1,5 @@
 import { PokeapiResponse } from '../interfaces/pokeapi-response.interface'
-import { PokeApiAdapter } from '../api/pokeApi.adapter'
+import { HttpAdapter, PokeApiAdapter, PokeApiFectchAdapter } from '../api/pokeApi.adapter'
 
 export class Pokemon {
     
@@ -9,18 +9,19 @@ export class Pokemon {
     constructor(
         public id: number,
         public name: string,
-        private readonly http: PokeApiAdapter
+        private readonly http: HttpAdapter
     ){}
 
     public getFr(){
         return 6
     }
     async getMoves(){
-        const {data} = await this.http.get('https://pokeapi.co/api/v2/pokemon/4')
+        const data = await this.http.get<PokeapiResponse>('https://pokeapi.co/api/v2/pokemon/4')
         return data.moves
     }
 }
 
 const pokeApi = new PokeApiAdapter()
-export const charmander = new Pokemon(1, 'pikachu', pokeApi)
+const pokeApiFetch = new PokeApiFectchAdapter()
+export const charmander = new Pokemon(1, 'pikachu', pokeApiFetch)
 console.log(await charmander.getMoves())
