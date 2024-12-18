14284c8844128181f10814e3eb394f5ce05ff9eb
Javier Flores <jffc.dev@gmail.com>
2024-12-18 14:01:07 -0500

    feat: insert clause added to seed

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
+  constructor(private readonly pokemonService: PokemonService) {}
+
   private readonly axios = axios;
 
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=20',
     );
 
-    data.results.forEach(({ name, url }) => {
-      const segments = url.split('/');
+    data.results.map((item) => {
+      const segments = item.url.split('/');
       const no = +segments[segments.length - 2];
     });
+
+    this.pokemonService.createMany(data.results);
+
     return data.results;
   }
 }
