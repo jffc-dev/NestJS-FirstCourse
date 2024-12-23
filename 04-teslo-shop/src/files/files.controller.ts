8526697056558e93106078de7873c0e7a32c1c5d
Javier Flores <jffc.dev@gmail.com>
2024-12-22 20:52:37 -0500

    feat(teslo): files module added

---
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -0,0 +1,33 @@
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      limits: { fileSize: 100000 },
+      storage: diskStorage({
+        destination: './static/uploads',
+      }),
+    }),
+  )
+  uploadedFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('make sure that the file is an image');
+    }
+    return file;
+  }
+}
